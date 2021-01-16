0a0fa97df4c228a44dc8e5182546a8999523f82b
Juan Aleman <juan.j.aleman@gmail.com>
2021-01-15 20:58:06 -0600

    Code: Navbar created and routed

--- demo-project/src/components/Navbar.js
+++ demo-project/src/components/Navbar.js
@@ -1,10 +1,31 @@
 import React from "react";
+import  { Link } from "react-router-dom";
+
 
 function Navbar() {
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className="navbar">
+            <div className="nav-center">
+                <Link to="/">
+                    {/* this h1 will be changed to img once I have a logo created */}
+                    <h1 className="logo">Logo Will Go here</h1>
+                </Link>
+                <ul className="nav-links">
+                    <li>
+                        <Link to="/">
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/about">
+                            About                        
+                        </Link>
+                    </li>
+
+                </ul>
+
+            </div>
+        </nav>
     )
 }
 
