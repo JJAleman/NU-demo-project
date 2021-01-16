9a0e78ed23b29ffbd93873869d273cb7f151ab5c
Juan Aleman <juan.j.aleman@gmail.com>
2021-01-15 21:25:23 -0600

    Code: About and error page content completed

--- demo-project/src/pages/About.js
+++ demo-project/src/pages/About.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 function About() {
-    return (
-        <div>
-            About Page
-        </div>
-    )
+  return (
+    <section className="section about-section">
+      <h1 className="section-title"> About us </h1>{" "}
+      <p>
+        loremNostrud exercitation commodo in amet sunt amet elit cupidatat laboris mollit laboris velit. Amet qui occaecat nulla laborum fugiat do sint velit. Minim velit reprehenderit Lorem sint deserunt. Sint consequat ut fugiat amet commodo est culpa ad ipsum.
+      </p>
+    </section>
+  );
 }
 
-export default About; 
+export default About;
--- demo-project/src/pages/Error.js
+++ demo-project/src/pages/Error.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 function Error() {
     return (
-        <div>
-            Error Page!
-        </div>
+        <section className="error-page section">
+            <div className="error-container">
+                <h1>Oops! It's a dead end!</h1>
+                <Link to="/" className="btn btn-primary">
+                    Back home
+                </Link>
+            </div>
+        </section>
     )
 }
 
