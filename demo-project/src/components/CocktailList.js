2df2fe1a54d006add1607c3eacc2029637007dcb
Juan Aleman <juan.j.aleman@gmail.com>
2021-01-16 17:51:20 -0600

    code: finished building the rest of the single cocktail page

--- demo-project/src/components/CocktailList.js
+++ demo-project/src/components/CocktailList.js
@@ -1,33 +1,28 @@
-import React from "react";
-import Cocktail from "./Cocktail";
-import Loading from "./Loading";
-import { useGlobalContext } from "../context";
+import React from 'react'
+import Cocktail from './Cocktail'
+import Loading from './Loading'
+import { useGlobalContext } from '../context'
 
-const CocktailList = () => {
-  const { cocktails, loading } = useGlobalContext();
-
+export default function CocktailList() {
+  const { cocktails, loading } = useGlobalContext()
   if (loading) {
-    return <Loading />
+    return <Loading/>
   }
-
-  if (cocktails.lenght < 1) {
+  if (cocktails.length < 1) {
     return (
-      <h2 className="seaction-title">
-        No cocktails match the search criteria.
+      <h2 className='section-title'>
+        no cocktails matched your search criteria
       </h2>
-    );
+    )
   }
-
   return (
-    <section className="section">
-      <h2 className="section-title">Cocktail List</h2>
-      <div className="cocktails-center">
+    <section className='section'>
+      <h2 className='section-title'>cocktails</h2>
+      <div className='cocktails-center'>
         {cocktails.map((item) => {
-          <Cocktail key={item.id} {...item} />;
+          return <Cocktail key={item.id} {...item} />
         })}
       </div>
     </section>
-  );
-};
-
-export default CocktailList;
+  )
+}
