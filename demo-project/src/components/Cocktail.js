641209ffefe1044bebb94e82b101cd33eead8521
Juan Aleman <juan.j.aleman@gmail.com>
2021-01-17 14:01:26 -0600

    bug: added missing $ for id

--- demo-project/src/components/Cocktail.js
+++ demo-project/src/components/Cocktail.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Cocktail = ({ image, name, id, info, glass }) => {
+export default function Cocktail({ image, name, id, info, glass }) {
   return (
     <article className="cocktail">
       <div className="img-container">
@@ -11,7 +11,7 @@ const Cocktail = ({ image, name, id, info, glass }) => {
         <h3>{name}</h3>
         <h4>{glass}</h4>
         <p>{info}</p>
-        <Link to={`/cocktail/{id}`} className="btn btn-primary btn-details">
+        <Link to={`/cocktail/${id}`} className="btn btn-primary btn-details">
             Details
         </Link>
       </div>
@@ -19,4 +19,3 @@ const Cocktail = ({ image, name, id, info, glass }) => {
   );
 };
 
-export default Cocktail;
